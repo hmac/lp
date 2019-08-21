6d7584bc28f22cb79946a1e94051839e29d1730b
Harry Maclean <harryjmaclean@gmail.com>
2019-08-21 16:57:13 +0100

    Remove code for evaluating BExprs
    We don't do this anymore, so we can remove this messy code.

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE LambdaCase #-}
-module Eval where
+module Eval
+  ( evalExpr
+  )
+where
 
 import           Data.Functor.Foldable
 import           Expr                    hiding ( Context )
@@ -37,85 +40,6 @@ evalExpr ctx ex = head (reduceList' ex)
     Fix (NatElim m mz ms (Fix (Suc n))) -> app (app ms n) (natElim m mz ms n)
     Fix (NatElim m mz ms k            ) -> natElim m mz ms (reduce' k)
 
-type Context = [BExpr]
-
-evalBExpr :: [BExpr] -> BExpr -> BExpr
-evalBExpr = nfc
-
-nfc :: Context -> BExpr -> BExpr
-nfc ctx e = head (reduceList ctx e)
-
-reduceList :: Context -> BExpr -> [BExpr]
-reduceList ctx expr = go [expr]
- where
-  go (e : es) =
-    let e' = reduce ctx e in if e' == e then e : es else go (e' : e : es)
-  go _ = [expr]
-
--- TODO: what about reducing applications of (Pi ...) x ?
-reduce :: Context -> BExpr -> BExpr
-reduce ctx (Fix expr) = case expr of
-  App (Fix l@Lam{}  ) b     -> breduce (Fix l) b
-  App (Fix p@Pi{}   ) b     -> breduce (Fix p) b
-  App (Fix (Ann e _)) b     -> reduce ctx (Fix (App e b))
-  App a               b     -> Fix $ App (reduce ctx a) (reduce ctx b)
-  Lam v               e     -> Fix $ Lam v (reduce ctx e)
-  Var i                     -> fromMaybe (Fix (Var i)) (safeIndex i ctx)
-  Ann e t                   -> Fix $ Ann (reduce ctx e) (reduce ctx t)
-  Pi x t e                  -> Fix $ Pi x (reduce ctx t) (reduce ctx e)
-  Type                      -> Fix Type
-  Nat                       -> Fix Nat
-  Zero                      -> Fix Zero
-  Suc n                     -> Fix (Suc (reduce ctx n))
-  NatElim _ mz _ (Fix Zero) -> mz
-  NatElim m mz ms (Fix (Suc n)) ->
-    Fix $ App (Fix (App ms n)) (Fix (NatElim m mz ms n))
-  NatElim m mz ms k -> Fix $ NatElim m mz ms (reduce ctx k)
-
--- TODO: this is so messy and complex - surely we can simplify it
-breduce :: BExpr -> BExpr -> BExpr
-breduce (Fix (Lam _ (Fix e))) (Fix b) =
-  -- Substitute matching variables
-  let e'  = sub b 0 e
-  -- Decrement free variables
-      e'' = decFree e'
-  in  Fix e''
-breduce (Fix (Pi _ _ (Fix t))) (Fix b) =
-  let t' = decFree (sub b 0 t) in Fix t'
-
-sub :: ExprF () Int BExpr -> Int -> ExprF () Int BExpr -> ExprF () Int BExpr
-sub b i (Var j) | i == j    = b
-                | otherwise = Var j
-sub b i (Lam v (Fix e)       )  = Lam v (Fix (sub b (i + 1) e))
-sub b i (Pi x (Fix t) (Fix e))  = Pi x (Fix (sub b i t)) (Fix (sub b (i + 1) e))
-sub _ _ Type                    = Type
-sub b i (Ann (Fix e ) (Fix t )) = Ann (Fix (sub b i e)) (Fix (sub b i t))
-sub b i (App (Fix e1) (Fix e2)) = App (Fix (sub b i e1)) (Fix (sub b i e2))
-sub _ _ Nat                     = Nat
-sub _ _ Zero                    = Zero
-sub b i (Suc (Fix n))           = Suc (Fix (sub b i n))
-sub b i (NatElim (Fix m) (Fix mz) (Fix ms) (Fix k)) = NatElim
-  (Fix (sub b i m))
-  (Fix (sub b i mz))
-  (Fix (sub b i ms))
-  (Fix (sub b i k))
-
-decFree :: ExprF () Int BExpr -> ExprF () Int BExpr
-decFree = go 0
- where
-  go i (Var j) | j > i     = Var (j - 1)
-               | otherwise = Var j
-  go i (Lam v (Fix e)       ) = Lam v (Fix (go (i + 1) e))
-  go i (Pi x (Fix t) (Fix e)) = Pi x (Fix (go i t)) (Fix (go (i + 1) e))
-  go _ Type                   = Type
-  go i (Ann (Fix e) (Fix t))  = Ann (Fix (go i e)) (Fix (go i t))
-  go i (App (Fix a) (Fix b))  = App (Fix (go i a)) (Fix (go i b))
-  go _ Nat                    = Nat
-  go _ Zero                   = Zero
-  go i (Suc (Fix n))          = Suc (Fix (go i n))
-  go i (NatElim (Fix m) (Fix mz) (Fix ms) (Fix k)) =
-    let r = Fix . go i in NatElim (r m) (r mz) (r ms) (r k)
-
 substitute :: String -> Expr -> Expr -> Expr
 substitute v a b = topDown' alg a
  where
@@ -135,13 +59,3 @@ topDown' :: Functor a => (Fix a -> Either (Fix a) (Fix a)) -> Fix a -> Fix a
 topDown' f a = case f a of
   Right b -> Fix (fmap (topDown' f) (unfix b))
   Left  b -> b
-
--- Not used - here for reference
-topDown :: Functor a => (Fix a -> Fix a) -> Fix a -> Fix a
-topDown f = Fix . fmap (topDown f) . unfix . f
-
--- TODO: move to utils
-safeIndex :: Int -> [a] -> Maybe a
-safeIndex _ []       = Nothing
-safeIndex 0 (x : _ ) = Just x
-safeIndex i (_ : xs) = safeIndex (i - 1) xs
